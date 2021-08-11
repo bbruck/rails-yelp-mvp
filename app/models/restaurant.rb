e7b54b39a0cf69c8f89ca5cb4654d3551779e0dd
Bernard Bruck <bernard.bruck@gmail.com>
2021-08-11 12:30:53 +0200

    Generate restaurants controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: '`%value` is not valid' }
 end
